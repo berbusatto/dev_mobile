784eea81ca367ad2d6f768448b102f38d9400fe5
Bernardo Busatto <berbusatto@gmail.com>
2022-08-11 20:30:22 -0300

    atualizando os resources/layout

--- app/src/main/java/qwerty/aulasMobileJava/MainActivity.java
+++ app/src/main/java/qwerty/aulasMobileJava/MainActivity.java
@@ -1,7 +1,19 @@
 package qwerty.aulasMobileJava;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Hello... World... bip bop", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main); // classe R é de resources, pasta res
+
+
+    }
 }
