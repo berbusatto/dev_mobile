39c8f9f32e93bbb31f8d83eca34e13a5b0086d61
Bernardo Busatto <berbusatto@gmail.com>
2022-08-16 20:38:55 -0300

    result view e control

--- app/src/main/java/qwerty/aulasMobileJava/MainActivity.java
+++ app/src/main/java/qwerty/aulasMobileJava/MainActivity.java
@@ -1,18 +1,51 @@
 package qwerty.aulasMobileJava;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
+    public static final String EXTRA_VALOR_UM = "VALOR_UM";
+    public static final String EXTRA_VALOR_DOIS = "VALOR_DOIS";
+
+    private Button botaoCalcular;
+    private EditText precoGasolina;
+    private EditText precoEtanol;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Hello... World... bip bop", Toast.LENGTH_LONG).show();
+
         setContentView(R.layout.activity_main); // classe R é de resources, pasta res
+
+        precoGasolina = findViewById(R.id.precoGasolina);
+        precoEtanol = findViewById(R.id.precoEtanol);
+        botaoCalcular = findViewById(R.id.botaoCalcular);
+
+        botaoCalcular.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeToResultScreen();
+            }
+        }));
+    }
+
+    private void changeToResultScreen() {
+        Intent intent = new Intent(this, ResultActivity.class); // INTENT É QUEM TRANSPORTA OS DADOS
+        // PARAMETROS DO INTENT(TELA ORIGEM, TELA DESTINO)
+        EditText precoGasolina = (EditText) findViewById(R.id.precoGasolina);
+        EditText precoEtanol = (EditText) findViewById(R.id.precoEtanol);
+
+        intent.putExtra(EXTRA_VALOR_UM, precoGasolina.getText().toString());
+        intent.putExtra(EXTRA_VALOR_DOIS, precoEtanol.getText().toString());
+        startActivity(intent);
 
 
     }
---
+++ app/src/main/java/qwerty/aulasMobileJava/ResultActivity.java
@@ -0,0 +1,48 @@
+package qwerty.aulasMobileJava;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import androidx.annotation.Nullable;
+
+public class ResultActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+        String precoGasolina = intent.getStringExtra(MainActivity.EXTRA_VALOR_UM);
+        String precoEtanol = intent.getStringExtra(MainActivity.EXTRA_VALOR_DOIS);
+
+        TextView resultadoView = findViewById(R.id.textoResultado);
+
+        double precoGasolinaBack = Double.parseDouble(precoGasolina);
+        double precoEtanolBack = Double.parseDouble(precoEtanol);
+        double resultado = precoEtanolBack / precoGasolinaBack;
+
+        if(resultado >= 0.7){
+            resultadoView.setText("Abasteça com Gasolina! A razão entre  é de " + resultado);
+        } else {
+            resultadoView.setText("Abasteça com Etanol! A razão entre alcool e gasolina é de " + resultado);
+
+        }
+
+        resultadoView.setText("O razão entre alcool e gasolina é de " + resultado);
+
+        Button backButton = findViewById(R.id.botaoVoltar);
+
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                ResultActivity.super.onBackPressed();
+            }
+        });
+    }
+}
